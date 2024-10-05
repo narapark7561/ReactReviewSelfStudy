11672d4c7f2373e601e5da43889c29e3d96905ed
narapark7561 <narapark7561@gmail.com>
2024-10-04 23:00:10 -0600

    Completed review with CSS styling.

--- awesomecarrie/app/week-5/managed-form.js
+++ awesomecarrie/app/week-5/managed-form.js
@@ -21,43 +21,83 @@ export default function RegistraionForm() {
     setSubscription(event.target.value);
   const handleMessageChange = (event) => setMessage(event.target.value);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let registraionObject = {
+      fName: fullName,
+      mail: email,
+      bDay: birthDate,
+      plan: subscription,
+      msg: message,
+    };
+
+    alert(`
+    Name: ${registraionObject.fName}
+    Email: ${registraionObject.mail}
+    Birthday: ${registraionObject.bDay}
+    Plan Type: ${registraionObject.plan}
+    Message: ${registraionObject.msg}
+    `);
+
+    setfullName("");
+    setEmail("");
+    setBirthDate("");
+    setMessage("");
+  };
+
+  const inputStyle =
+    "mb-3, px-2 py-1 rounded border-2 border-blue-500 focus:bg-orange-200";
+  const containerStyle = "mb-3";
+  const labelStyle = "inline-block w-40";
+
   return (
-    <form>
-      <div>
-        <label>Full Name: </label>
+    <form onSubmit={handleSubmit} className="p-4 bg-lime-500">
+      <div className={containerStyle}>
+        <label className={labelStyle}>Full Name: </label>
         <input
           onChange={handleFullNameChange}
           value={fullName}
           type="text"
           name="full_name"
           placeholder="Chai Park"
+          className={inputStyle}
         />
       </div>
-      <div>
-        <label>Email: </label>
+      <div className={containerStyle}>
+        <label className={labelStyle}>Email: </label>
         <input
           onChange={handleEmailChange}
           value={email}
           type="email"
           placeholder="chai@cute.ca"
+          className={inputStyle}
         />
       </div>
-      <div>
-        <label>Birthday: </label>
-        <input onChange={handleBirthDateChange} value={birthDate} type="date" />
+      <div className={containerStyle}>
+        <label className={labelStyle}>Birthday: </label>
+        <input
+          onChange={handleBirthDateChange}
+          value={birthDate}
+          type="date"
+          className={inputStyle}
+        />
       </div>
-      <div>
-        <label>Subscription Type: </label>
-        <select onChange={handleSubscriptionChange}>
+      <div className={containerStyle}>
+        <label className={labelStyle}>Subscription Type: </label>
+        <select onChange={handleSubscriptionChange} className={inputStyle}>
           <option>Please select the plan</option>
           <option>Free Plan</option>
           <option>Basic Plan</option>
           <option>Premium Plan</option>
         </select>
       </div>
-      <div>
-        <label>Message: </label>
-        <textarea onChange={handleMessageChange} value={message}></textarea>
+      <div className={containerStyle}>
+        <label className={labelStyle}>Message: </label>
+        <textarea
+          onChange={handleMessageChange}
+          value={message}
+          className={inputStyle}
+        ></textarea>
       </div>
       <button className="bg-green-500 hover:bg-green-800 active:bg-green-700 text-white rouded px-3 py-2 ">
         Submit Registration
