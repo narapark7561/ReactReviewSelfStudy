487740b9831d94cfe5942b336390a45ba47167d0
narapark7561 <narapark7561@gmail.com>
2024-10-04 23:06:45 -0600

    Completed review for toggle form

--- awesomecarrie/app/week-5/page.js
+++ awesomecarrie/app/week-5/page.js
@@ -1,10 +1,27 @@
+"use client";
+
+import { useState } from "react";
 import RegistraionForm from "./managed-form";
 
 export default function RegistraionPage() {
+  const [formOpen, setformOpen] = useState(false);
+  const toggleForm = () => {
+    if (formOpen === true) {
+      setformOpen(false);
+    } else {
+      setformOpen(true);
+    }
+  };
+
+  //   const toggleForm2 = () =>
+  //     formOpen === true ? setformOpen(false) : setformOpen(true);
+  const toggleForm2 = () => (formOpen ? setformOpen(false) : setformOpen(true));
+
   return (
     <main>
       <h1>Registration</h1>
-      <RegistraionForm />
+      <button onClick={toggleForm2}>Click!</button>
+      {formOpen && <RegistraionForm />}
     </main>
   );
 }
